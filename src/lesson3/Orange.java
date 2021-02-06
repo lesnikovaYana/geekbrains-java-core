363372c4bb4577428817dd0b3c53b2d1591a89f1
Yana <lesnikova.yana@yandex.ru>
2021-02-06 15:28:09 +0300

    homework3 тяжко.

---
+++ src/lesson3/Orange.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Orange extends Fruit{
+    public Orange(float weight) {
+        super(weight);
+    }
+
+    @Override
+    public float getWeight() {
+        return super.getWeight();
+    }
+}
